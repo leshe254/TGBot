42eb3b495c494de8a386aa8838d89cd244f9f84a
leshe254 <babikov727@gmail.com>
2024-02-21 13:13:03 +0300

    Added some if clauses and return methods to choose correct answers

--- bot.py
+++ bot.py
@@ -5,50 +5,78 @@ from tokenbot import token
 
 bot=telebot.TeleBot(token)
 
+departments = ["Администрация", "Администрация эл. журнала", "IT отдел", "Завхоз"]
+criticals = ["Жизненно-необходимо", "Средняя важность", "В свободное время"]
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     user_first_name = str(message.chat.first_name)
     user_nik = str(message.from_user.username)
     bot.send_message(message.chat.id, f"Здравствуйте, {user_first_name}!") #Приветствие собеседника!
     
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Администрация")
-    item2=types.KeyboardButton("Администрация эл. журнала")
-    item3=types.KeyboardButton("IT отдел")
-    item4=types.KeyboardButton("Завхоз")
-    markup.add(item1)
-    markup.add(item2)
-    markup.add(item3)
-    markup.add(item4)
+    if(len(departments) > 0): #Проверяем количество элементов массива "отделы"
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for i in range(len(departments)): #Для каждого элемента перечня создаем кнопку
+            itemtmp=types.KeyboardButton(departments[i])
+            markup.add(itemtmp)
     bot.send_message(message.chat.id,'Выберите к кому хотите обратиться',reply_markup=markup)
-    bot.register_next_step_handler(message, critical_switch);
+    bot.register_next_step_handler(message, critical_switch)
     
     #Сценарий на все случаи жизни)))
 def critical_switch(message):
         dep = str(message.text)
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        crit1=types.KeyboardButton("Жизненно-необходимо")
-        crit2=types.KeyboardButton("Средняя важность")
-        crit3=types.KeyboardButton("В свободное время")
-        markup.add(crit1)
-        markup.add(crit2)
-        markup.add(crit3)
-        bot.send_message(message.chat.id,'Укажите приоритетность вашего обращения',reply_markup=markup)
-        bot.register_next_step_handler(message, cabinet_input);
+        if(dep in departments): #Проверка на дурака
+            if(len(criticals) > 0): #Проверяем количество элементов массива "критичность задачи"
+                markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+                for i in range(len(criticals)): #Для каждого элемента перечня создаем кнопку
+                    itemtmp=types.KeyboardButton(criticals[i])
+                    markup.add(itemtmp)
+            bot.send_message(message.chat.id,'Укажите приоритетность вашего обращения',reply_markup=markup)
+            bot.register_next_step_handler(message, cabinet_input)
+        else:
+            if(len(departments) > 0): #Проверяем количество элементов массива "отделы"
+                markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+                for i in range(len(departments)): #Для каждого элемента перечня создаем кнопку
+                    itemtmp=types.KeyboardButton(departments[i])
+                    markup.add(itemtmp)
+            bot.send_message(message.chat.id,'Выберите из списка!',reply_markup=markup)
+            bot.register_next_step_handler(message, critical_switch)
+        
 
 def cabinet_input(message):
-        bot.send_message(message.chat.id,'Введите номер кабинета', reply_markup=types.ReplyKeyboardRemove())
-        bot.register_next_step_handler(message, problem);
+        crit = str(message.text)
+        if(crit in criticals): #Проверка на дурака
+            bot.send_message(message.chat.id,'Укажите кабинет', reply_markup=types.ReplyKeyboardRemove())
+            bot.register_next_step_handler(message, problem)
+        else:
+            if(len(criticals) > 0): #Проверяем количество элементов массива "критичность задачи"
+                markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+                for i in range(len(criticals)): #Для каждого элемента перечня создаем кнопку
+                    itemtmp=types.KeyboardButton(criticals[i])
+                    markup.add(itemtmp)
+            bot.send_message(message.chat.id,'Выберите из списка!',reply_markup=markup)
+            bot.register_next_step_handler(message, cabinet_input)
         
 def problem(message):
-        bot.send_message(message.chat.id,'Опишите Вашу проблему')
-        bot.register_next_step_handler(message, finish_message);
+        cab = str(message.text)
+        if(cab[0] != '/'): #Проверка на дурака
+            bot.send_message(message.chat.id,'Опишите Вашу проблему')
+            bot.register_next_step_handler(message, finish_message)
+        else:
+            bot.send_message(message.chat.id,'Недопустимая команда, попробуйте указать кабинет без "/"')
+            bot.register_next_step_handler(message, problem)
         
 def finish_message(message):
-        time = datetime.now().strftime("%a, %d %b %Y %H:%M:%S") #Окончательное время регистрации заявки
-        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        new_problem=types.KeyboardButton("/start")
-        markup.add(new_problem)
-        bot.send_message(message.chat.id,'Спасибо за обращение, информация передана! Чтобы зарегистрировать новое обращение - нажмите "/start"',reply_markup=markup)
-        #Тут должна быть отправка данных на сервер
+        prob = str(message.text)
+        if(prob[0] != '/'): #Проверка на дурака
+            time = datetime.now().strftime("%a, %d %b %Y %H:%M:%S") #Окончательное время регистрации заявки
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            new_problem=types.KeyboardButton("/start")
+            markup.add(new_problem)
+            bot.send_message(message.chat.id,'Спасибо за обращение, информация передана! Чтобы зарегистрировать новое обращение - нажмите "/start"',reply_markup=markup)
+            #Тут должна быть отправка данных на сервер
+        else:
+            bot.send_message(message.chat.id,'Недопустимая команда, попробуйте начать описание не с "/"')
+            bot.register_next_step_handler(message, finish_message) 
+
 bot.infinity_polling()
