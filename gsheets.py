0d6a48690c3c24e3d43d4275ccdd1016d75357a7
leshe254 <babikov727@gmail.com>
2024-02-22 15:52:06 +0300

    Some fixing

--- gsheets.py
+++ gsheets.py
@@ -1,22 +1,29 @@
 import gspread
 from datetime import datetime
 
-#Двумерный массив с сопоставлением отдела и ссылки на таблицу
-sheets = [["Администрация", "https://docs.google.com/spreadsheets/d/1H00lV5YvAA6wVHLankgA8zp5jHb0ktjMsi9aVrFiJRo"],
-["Администрация эл. журнала" , "https://docs.google.com/spreadsheets/d/1Fsj8fp93V_R0rLY_xtUeMSKWrqQv5N1Igiy1kTrb9Iw"],
-["IT отдел", "https://docs.google.com/spreadsheets/d/14NnC9nyCtADkD81q9RMFJKFfNz3RTo_egr3eO8GUQzk"],
-["Завхоз", "https://docs.google.com/spreadsheets/d/17ZJ6i08O_Ebg2py6FUHqul2Bp-rjbRxUq41mDrHhpN0"]]
+# Двумерный массив с сопоставлением отдела и ссылки на таблицу
+sheets = [
+    ["Администрация", "https://docs.google.com/spreadsheets/d/1H00lV5YvAA6wVHLankgA8zp5jHb0ktjMsi9aVrFiJRo"],
+    [
+        "Администрация эл. журнала",
+        "https://docs.google.com/spreadsheets/d/1Fsj8fp93V_R0rLY_xtUeMSKWrqQv5N1Igiy1kTrb9Iw",
+    ],
+    ["IT отдел", "https://docs.google.com/spreadsheets/d/14NnC9nyCtADkD81q9RMFJKFfNz3RTo_egr3eO8GUQzk"],
+    ["Завхоз", "https://docs.google.com/spreadsheets/d/17ZJ6i08O_Ebg2py6FUHqul2Bp-rjbRxUq41mDrHhpN0"],
+]
 
-#Привязка токена
+# Привязка токена
 gc = gspread.service_account("tokengoogle.json")
 
-#Распределение заявок по таблицам отделов и добавление в таблицы новой строки
+# Распределение заявок по таблицам отделов и добавление в таблицы новой строки
 def senddata(dep, user_nik, cabinet, critical, problem):
     for i in range(0, len(sheets)):
-        if(dep == sheets[i][0]):
-            #Окончательное время регистрации заявки
-            time = datetime.now().strftime("%d %b %Y, %H:%M") 
-            #Функция подключения к таблице и получение первого листа
+        if dep == sheets[i][0]:
+            # Окончательное время регистрации заявки
+            time = datetime.now().strftime("%d %b %Y, %H:%M")
+            # Функция подключения к таблице и получение первого листа
             sh = gc.open_by_url(sheets[i][1]).get_worksheet(0)
-            sh.append_row([time, user_nik, cabinet, critical, problem],value_input_option='RAW',insert_data_option=None)
+            sh.append_row(
+                [time, user_nik, cabinet, critical, problem], value_input_option='RAW', insert_data_option=None
+            )
             print(f"{time}: Оставлена новая заявка в отдел {dep}!")
