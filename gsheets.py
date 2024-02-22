f55734a2384814cb7186ce5abc0362009336f596
leshe254 <babikov727@gmail.com>
2024-02-22 14:22:05 +0300

    Fixed for clause

--- gsheets.py
+++ gsheets.py
@@ -12,7 +12,7 @@ gc = gspread.service_account("tokengoogle.json")
 
 #Распределение заявок по таблицам отделов и добавление в таблицы новой строки
 def senddata(dep, user_nik, cabinet, critical, problem):
-    for i in range(0, 4):
+    for i in range(0, len(sheets)):
         if(dep == sheets[i][0]):
             #Окончательное время регистрации заявки
             time = datetime.now().strftime("%d %b %Y, %H:%M") 
