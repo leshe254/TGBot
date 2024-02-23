7a0e44baadd33530b6f2c11f6d8b1e1d966010cf
leshe254 <babikov727@gmail.com>
2024-02-23 21:26:45 +0300

    Added worktime to the bot (Days of week and hours)

--- bot.py
+++ bot.py
@@ -2,83 +2,125 @@ import sys
 import telebot
 from tokenbot import token
 from gsheets import senddata
+from datetime import datetime
 
-bot = telebot.TeleBot(token)
+# Задаем токен для телебота и игнорируем висячие сообщения
+bot = telebot.TeleBot(token, skip_pending=True)
 
-# Сеты с отделами и критичностью задач
+# Листы с отделами и критичностью задач
 departments = ["Администрация", "Администрация эл. журнала", "IT отдел", "Завхоз"]
 criticals = ["Жизненно-необходимо", "Средняя важность", "В свободное время", "Вернуться назад"]
+# Параметры, которые передаются дальше
+datalist = []
+# Режим работы
+workdays = {'Mon', 'Tue', 'Wed', 'Thu', 'Fri'}
+workhours = ['8', '20']
 
-@bot.message_handler(commands=['start'])
+# Проверка рабочего времени бота
+def check_worktime():
+    weekday = datetime.now().strftime("%a")
+    dayhour = datetime.now().strftime("%H")
+    if(weekday in workdays and int(dayhour) > int(workhours[0]) and int(dayhour) < int(workhours[1])):
+        return True
+    else:
+        return False
+
+
+@bot.message_handler(content_types=['text'])
 def start_message(message):
-    user_nik = str(message.from_user.username)
-    # Приветствие собеседника!
-    bot.send_message(message.chat.id, f"Здравствуйте, {message.chat.first_name}!")
-    bot.send_message(message.chat.id, 'Выберите к кому хотите обратиться', reply_markup=depmarkup)
-    bot.register_next_step_handler(message, critical_switch, user_nik)
-
-    # Сценарий на все случаи жизни)))
-
-
-def critical_switch(message, user_nik):
-    dep = str(message.text)
-    # Проверка на дурака
-    if dep in departments:
-        bot.send_message(message.chat.id, 'Укажите приоритетность вашего обращения', reply_markup=critmarkup)
-        bot.register_next_step_handler(message, cabinet_input, user_nik, dep)
-    else:
-        bot.send_message(message.chat.id, 'Выберите из списка!', reply_markup=depmarkup)
-        bot.register_next_step_handler(message, critical_switch, user_nik)
-
-
-def cabinet_input(message, user_nik, dep):
-    crit = str(message.text)
-    # Проверка на дурака
-    if crit in criticals:
-        if (crit == "Вернуться назад"):
+    if(check_worktime()):
+        if(str(message.text) != "/start"):
+            bot.send_message(message.chat.id, "Вероятнее всего возникла проблема, попробуйте начать сначала", reply_markup=startmarkup)
+        else:
+            user_nik = str(message.from_user.username)
+            # Приветствие собеседника!
+            bot.send_message(message.chat.id, f"Здравствуйте, {message.chat.first_name}!")
             bot.send_message(message.chat.id, 'Выберите к кому хотите обратиться', reply_markup=depmarkup)
-            bot.register_next_step_handler(message, critical_switch, user_nik)
-        else:
-            bot.send_message(message.chat.id, 'Укажите кабинет', reply_markup=backmarkup)
-            bot.register_next_step_handler(message, problem, user_nik, dep, crit)
+            datalist.extend(user_nik)
+            bot.register_next_step_handler(message, critical_switch, datalist)
     else:
-        bot.send_message(message.chat.id, 'Выберите из списка!', reply_markup=critmarkup)
-        bot.register_next_step_handler(message, cabinet_input, user_nik, dep)
+        bot.send_message(message.chat.id, "Заявки принимаются только в рабочее время!\n(Пн-Пт с 8:00 до 20:00)", reply_markup=startmarkup)
 
 
-def problem(message, user_nik, dep, crit):
-    cab = str(message.text)
-    # Проверка на дурака
-    if cab[0] != '/':
-        if (cab == "Вернуться назад"):
+
+def critical_switch(message, datalist):
+    if(check_worktime()):
+        dep = str(message.text)
+        # Проверка на дурака
+        if dep in departments:
             bot.send_message(message.chat.id, 'Укажите приоритетность вашего обращения', reply_markup=critmarkup)
-            bot.register_next_step_handler(message, cabinet_input, user_nik, dep)
+            datalist.extend(dep)
+            bot.register_next_step_handler(message, cabinet_input, datalist)
         else:
-            bot.send_message(message.chat.id, 'Опишите Вашу проблему')
-            bot.register_next_step_handler(message, problem_message, user_nik, dep, crit, cab)
+            bot.send_message(message.chat.id, 'Выберите из списка!', reply_markup=depmarkup)
+            bot.register_next_step_handler(message, critical_switch, datalist)
     else:
-        bot.send_message(message.chat.id, 'Недопустимая команда, попробуйте указать кабинет без "/"')
-        bot.register_next_step_handler(message, problem, user_nik, dep, crit)
+        bot.send_message(message.chat.id, "Заявки принимаются только в рабочее время!\n(Пн-Пт с 8:00 до 20:00)", reply_markup=startmarkup)
+        bot.register_next_step_handler(message, start_message)
 
 
-def problem_message(message, user_nik, dep, crit, cab):
-    prob = str(message.text)
-    # Проверка на дурака
-    if prob[0] != '/':
-        if (prob == "Вернуться назад"):
-            bot.send_message(message.chat.id, 'Укажите кабинет', reply_markup=backmarkup)
-            bot.register_next_step_handler(message, problem, user_nik, dep, crit)
+def cabinet_input(message, datalist):
+    if(check_worktime()):
+        crit = str(message.text)
+        # Проверка на дурака
+        if crit in criticals:
+            if crit == "Вернуться назад":
+                bot.send_message(message.chat.id, 'Выберите к кому хотите обратиться', reply_markup=depmarkup)
+                bot.register_next_step_handler(message, critical_switch, datalist)
+            else:
+                bot.send_message(message.chat.id, 'Укажите кабинет', reply_markup=backmarkup)
+                datalist.extend(crit)
+                bot.register_next_step_handler(message, problem, datalist)
         else:
-            bot.send_message(
-                message.chat.id,
-                'Спасибо за обращение, информация передана! Чтобы зарегистрировать новое обращение - нажмите "/start"',
-                reply_markup=startmarkup,
-            )
-            # Вызываем метод передачи данных в Гугл таблицы
-            senddata(user_nik, dep, crit, cab, prob)
+            bot.send_message(message.chat.id, 'Выберите из списка!', reply_markup=critmarkup)
+            bot.register_next_step_handler(message, cabinet_input, datalist)
     else:
-        bot.send_message(message.chat.id, 'Недопустимая команда, попробуйте начать описание не с "/"')
-        bot.register_next_step_handler(message, problem_message, user_nik, dep, crit, cab)
+        bot.send_message(message.chat.id, "Заявки принимаются только в рабочее время!\n(Пн-Пт с 8:00 до 20:00)", reply_markup=startmarkup)
+        bot.register_next_step_handler(message, start_message)
+
+def problem(message, datalist):
+    if(check_worktime()):    
+        cab = str(message.text)
+        # Проверка на дурака
+        if cab[0] != '/':
+            if cab == "Вернуться назад":
+                bot.send_message(message.chat.id, 'Укажите приоритетность вашего обращения', reply_markup=critmarkup)
+                bot.register_next_step_handler(message, cabinet_input, datalist)
+            else:
+                bot.send_message(message.chat.id, 'Опишите Вашу проблему')
+                datalist.extend(cab)
+                bot.register_next_step_handler(message, problem_message, datalist)
+        else:
+            bot.send_message(message.chat.id, 'Недопустимая команда, попробуйте указать кабинет без "/"')
+            bot.register_next_step_handler(message, problem, datalist)
+    else:
+        bot.send_message(message.chat.id, "Заявки принимаются только в рабочее время!\n(Пн-Пт с 8:00 до 20:00)", reply_markup=startmarkup)
+        bot.register_next_step_handler(message, start_message)
+
+
+def problem_message(message, datalist):
+    if(check_worktime()):
+        prob = str(message.text)
+        # Проверка на дурака
+        if prob[0] != '/':
+            if prob == "Вернуться назад":
+                bot.send_message(message.chat.id, 'Укажите кабинет', reply_markup=backmarkup)
+                bot.register_next_step_handler(message, problem, datalist)
+            else:
+                bot.send_message(
+                    message.chat.id,
+                    'Спасибо за обращение, информация передана! Чтобы зарегистрировать новое обращение - нажмите "/start"',
+                    reply_markup=startmarkup,
+                )
+                datalist.extend(prob)
+                # Вызываем метод передачи данных в Гугл таблицы
+                senddata(datalist)
+        else:
+            bot.send_message(message.chat.id, 'Недопустимая команда, попробуйте начать описание не с "/"')
+            bot.register_next_step_handler(message, problem_message, datalist)
+    else:
+        bot.send_message(message.chat.id, "Заявки принимаются только в рабочее время!\n(Пн-Пт с 8:00 до 20:00)", reply_markup=startmarkup)
+        bot.register_next_step_handler(message, start_message)
 
 
 if __name__ == '__main__':
@@ -104,6 +146,6 @@ if __name__ == '__main__':
     backmarkup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     backbtn = telebot.types.KeyboardButton("Вернуться назад")
     backmarkup.add(backbtn)
-    
+
     # Запуск бота
     bot.infinity_polling()
--- gsheets.py
+++ gsheets.py
@@ -17,14 +17,17 @@ gc = gspread.service_account("tokengoogle.json")
 
 
 # Распределение заявок по таблицам отделов и добавление в таблицы новой строки
-def senddata(user_nik, dep, crit, cab, prob):
+def senddata(datalist):
+    user_nik = datalist[0]
+    dep = datalist [1]
+    crit = datalist[2]
+    cab = datalist[3]
+    prob = datalist[4]
     for i in range(0, len(sheets)):
         if dep == sheets[i][0]:
             # Окончательное время регистрации заявки
             time = datetime.now().strftime("%d %b %Y, %H:%M")
             # Функция подключения к таблице и получение первого листа
             sh = gc.open_by_url(sheets[i][1]).get_worksheet(0)
-            sh.append_row(
-                [time, user_nik, cab, crit, prob], value_input_option='RAW', insert_data_option=None
-            )
+            sh.append_row([time, user_nik, cab, crit, prob], value_input_option='RAW', insert_data_option=None)
             print(f"{time}: Оставлена новая заявка в отдел {dep}!")
